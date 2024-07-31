431fe5fc414bb917accfbcbb584ec48fe975132f
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2024-07-31 18:56:17 +0100

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         proxy: {
-            "/api": "taptap-production.up.railway.app",
+            "/api": "https://taptap-production.up.railway.app",
         },
     },
     plugins: [react()],

4fe27323c0a042eb587a3731f5529629124904bc
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2024-08-11 13:14:42 +0100

    Update Leaderboard.js

--- back-end/controllers/Leaderboard.js
+++ back-end/controllers/Leaderboard.js
@@ -16,9 +16,10 @@ async function allrank(req, res, next) {
         // Query to get all users' ranks
         console.log("Executing query to get top users");
         const [results] = await sequelize.query(`
-            SELECT e.tap_score, t.username, e.game_level, e.id, t.first_name 
-            FROM tg_users AS t
-            JOIN earnings AS e ON t.userid = e.id AND e.tap_score != 0
+            SELECT e.tap_score, t.username, e.game_level, e.id, t.first_name
+            FROM tg_users t
+            JOIN earnings e ON t.userid = e.userid
+            WHERE e.tap_score != 0;
             ORDER BY e.tap_score DESC
         `);
 
@@ -48,7 +49,7 @@ async function allrank(req, res, next) {
             const [specificUserResults] = await sequelize.query(`
                 SELECT e.tap_score, t.username, e.game_level, e.id, t.first_name 
                 FROM tg_users AS t
-                JOIN earnings AS e ON t.userid = e.id
+                JOIN earnings AS e ON t.userid = e.userid
                 WHERE t.userid = ?
                 ORDER BY e.tap_score DESC
             `, { replacements: [tid], type: sequelize.QueryTypes.SELECT });

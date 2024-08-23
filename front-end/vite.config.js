a244d17bbae108f7badea2d008de101561a55163
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2024-08-24 00:00:05 +0100

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://taptap-production.up.railway.app',
+        target: 'https://tap-genie-app-production.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
